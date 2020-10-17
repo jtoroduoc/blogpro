ea37573f6d742031ad202fe13ff7919d80cad55b
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2020-10-17 13:39:57 -0300

    Template de List y Detail OK!

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.noticia_list, name='noticia_list'),
-
+    path('noticia/<int:pk>/', views.noticia_detail, name='noticia_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Noticia
 from django.utils import timezone
 
 # Create your views here.
 
 def noticia_list(request):
-    noticias = Noticia.objects.all()
+    noticias = Noticia.objects.filter(publicacion__lte=timezone.now()).order_by('-publicacion')
     return render(request, 'news/noticia_list.html', {'noticias': noticias})
+
+def noticia_detail(request, pk):
+    noticia = get_object_or_404(Noticia, pk=pk)
+    return render(request, 'news/noticia_detail.html', {'noticia': noticia})
